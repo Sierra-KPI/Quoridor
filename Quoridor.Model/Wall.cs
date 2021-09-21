b52e9523aa428a3f438706df767410edb59b5f6d
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-09-21 21:56:02 +0300

    ✨: add Wall

---
+++ Quoridor.Model/Wall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quoridor.Model
+{
+	internal class Wall: IElement
+	{
+		public Position Position { get; set; }
+
+		public bool Place(Position position);
+	}
+}
