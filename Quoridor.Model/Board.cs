56011a767747427c711f09f6e8ac87c3409316ab
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-09-22 13:13:45 +0300

    :sparkles:: add method CreateBoard

--- Quoridor.Model/Board.cs
+++ Quoridor.Model/Board.cs
@@ -6,5 +6,18 @@ using System.Threading.Tasks;
 
 namespace Quoridor.Model {
     public class Board {
+
+        private Cell[,] cells;
+        private Cell[] validPlayersSpots;
+        private Cell[,] validWallsSpots;
+
+        public int width => cells.GetLength(0);
+        public int height => cells.GetLength(1);
+
+        public Board(Cell[,] cells) {
+            this.cells = cells;
+
+        }
+
     }
 }
--- Quoridor.Model/BoardFactory.cs
+++ Quoridor.Model/BoardFactory.cs
@@ -7,12 +7,30 @@ using System.Threading.Tasks;
 namespace Quoridor.Model {
 
     public interface IBoardFactory {
+
         Board CreateBoard();
+
     }
 
     public class BoardFactory : IBoardFactory {
+
+        private int size = 9;
+
         public Board CreateBoard() {
-            Board board = new Board();
+
+            var cells = new Cell[size, size];
+
+            var id = 0;
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    var position = new Position(i, j);
+                    cells[i, j] = new Cell(position);
+                    cells[i, j].id = id++;
+
+                }
+            }
+
+            var board = new Board(cells);
             return board;
         }
     }
