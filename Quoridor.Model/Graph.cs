63fe94143084814ecc7239610c0cc29e356f0539
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-09-23 20:53:33 +0300

    :sparkles:: remove one Graph constructor

--- Quoridor.Model/Graph.cs
+++ Quoridor.Model/Graph.cs
@@ -16,26 +16,9 @@ namespace Quoridor.Model {
             this.size = size;
             this.edges = edges;
 
-            this.distances = new int[this.size];
-            for (int i = 0; i < this.size; i++) {
-                distances[i] = int.MaxValue;
-            }
         }
 
-        public Graph(Cell[,] cells, int size) {
-            this.size = size;
-            this.edges = new int[cells.GetLength(0), 2];
-            for (int i = 0; i < cells.GetLength(0); i++) {
-                for (int j = 0; j < 2; j++) {
-                    this.edges[i, j] = cells[i, j].id;
-                }
-            }
-
-            this.distances = new int[this.size];
-            for (int i = 0; i < this.size; i++) {
-                distances[i] = int.MaxValue;
-            }
-        }
+        
 
         
 
