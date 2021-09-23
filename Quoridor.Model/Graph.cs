93991386ae04abb4195864ab31969cabf4aea46b
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-09-23 21:24:38 +0300

    :sparkles:: implement dijakstra algorithm

--- Quoridor.Model/Graph.cs
+++ Quoridor.Model/Graph.cs
@@ -10,9 +10,6 @@ namespace Quoridor.Model {
         int[,] edges;
         LinkedList<int>[] adjacencyList;
 
-        int min = int.MaxValue;
-        int[] distances;
-
         public Graph(int[,] edges, int size) {
             this.size = size;
             this.edges = edges;
@@ -20,29 +17,54 @@ namespace Quoridor.Model {
             makeAdjacencyList();
         }
 
-
         void makeAdjacencyList() {
-            this.adjacencyList = new LinkedList<int>[this.size];
+            adjacencyList = new LinkedList<int>[this.size];
 
             for (int i = 0; i < adjacencyList.Length; ++i) {
-                this.adjacencyList[i] = new LinkedList<int>();
+                adjacencyList[i] = new LinkedList<int>();
             }
 
             for (int i = 0; i < this.edges.GetLength(0); i++) {
-                this.addEdge(this.edges[i, 0], this.edges[i, 1]);
-                this.addEdge(this.edges[i, 1], this.edges[i, 0]);
+                addEdge(edges[i, 0], edges[i, 1]);
+                addEdge(edges[i, 1], edges[i, 0]);
             }
 
         }
 
         void addEdge(int v1, int v2) {
-            this.adjacencyList[v1].AddLast(v2);
+            adjacencyList[v1].AddLast(v2);
         }
 
         bool removeEdge(int v1, int v2) {
             return adjacencyList[v1].Remove(v2);
         }
 
+        public int[] dijkstraAlgorithm(int startVertex) {
+            int[] distances = new int[size];
+            bool[] used = new bool[size];
+
+            for (int i = 0; i < size; i++) {
+                distances[i] = int.MaxValue;
+            }
+            distances[startVertex] = 0;
+
+            for (int i = 0; i < size; i++) {
+                int curr = -1;
+                for (int j = 0; j < size; j++)
+                    if (!used[j] && (curr == -1 || distances[j] < distances[curr]))
+                        curr = j;
+
+                if (distances[curr] == int.MaxValue) break;
+                used[curr] = true;
+
+                LinkedList<int> list = adjacencyList[curr];
+                foreach (int vertex in list)
+                    if (distances[curr] + 1 < distances[vertex])
+                        distances[vertex] = distances[curr] + 1;
+
+            }
+            return distances;
+        }
 
 
 
