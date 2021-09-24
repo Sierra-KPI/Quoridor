3f3c47318dc5315d8d0db1dacc3824c15d7ad234
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-09-24 14:54:46 +0300

    :sparkles:: improve style with the help of @VsIG-official

--- Quoridor.Model/Graph.cs
+++ Quoridor.Model/Graph.cs
@@ -1,88 +1,106 @@
 ﻿using System.Collections.Generic;
 
-namespace Quoridor.Model {
-    class Graph {
-        private int size;
-        private int[,] edges;
-        private LinkedList<int>[] adjacencyList;
+namespace Quoridor.Model 
+{
+    class Graph 
+    {
+        private int _size;
+        private int[,] _edges;
+        private LinkedList<int>[] _adjacencyList;
 
-        public Graph(int _size) {
-            this.size = _size * _size;
-            this.edges = new int[2 * _size * (_size - 1), 2];
+        public Graph(int size) 
+        {
+            _size = size * size;
+            _edges = new int[2 * size * (size - 1), 2];
 
-            int idW = 0;
-            for (int v = 0; v < size; v++) {
-                if (v - 1 >= 0 && (v % _size != 0)) {
-                    edges[idW, 0] = v - 1;
-                    edges[idW, 1] = v;
-                    idW++;
+            int edgeId = 0;
+            for (var v = 0; v < size; v++) 
+            {
+                if (v - 1 >= 0 && (v % size != 0)) 
+                {
+                    _edges[edgeId, 0] = v - 1;
+                    _edges[edgeId, 1] = v;
+                    edgeId++;
                 }
-                if (v - 3 >= 0) {
-                    edges[idW, 0] = v - 3;
-                    edges[idW, 1] = v;
-                    idW++;
+                if (v - 3 >= 0) 
+                {
+                    _edges[edgeId, 0] = v - 3;
+                    _edges[edgeId, 1] = v;
+                    edgeId++;
                 }
             }
 
-            makeAdjacencyList();
+            MakeAdjacencyList();
         }
 
-        public Graph(int[,] edges, int size) {
-            this.size = size;
-            this.edges = edges;
+        public Graph(int[,] edges, int size) 
+        {
+            _size = size;
+            _edges = edges;
 
-            makeAdjacencyList();
+            MakeAdjacencyList();
         }
 
-        void makeAdjacencyList() {
-            adjacencyList = new LinkedList<int>[size];
+        void MakeAdjacencyList() 
+        {
+            _adjacencyList = new LinkedList<int>[_size];
 
-            for (int i = 0; i < adjacencyList.Length; ++i) {
-                adjacencyList[i] = new LinkedList<int>();
+            for (var i = 0; i < _adjacencyList.GetLength(0); ++i) 
+            {
+                _adjacencyList[i] = new LinkedList<int>();
             }
 
-            for (int i = 0; i < edges.GetLength(0); i++) {
-                addEdge(edges[i, 0], edges[i, 1]);
-                addEdge(edges[i, 1], edges[i, 0]);
+            for (var i = 0; i < _edges.GetLength(0); i++) 
+            {
+                AddEdge(_edges[i, 0], _edges[i, 1]);
+                AddEdge(_edges[i, 1], _edges[i, 0]);
             }
 
         }
 
-        void addEdge(int vertex1, int vertex2) {
-            adjacencyList[vertex1].AddLast(vertex2);
+        bool AddEdge(int vertex1, int vertex2) 
+        {
+            if (_adjacencyList[vertex1].AddLast(vertex2) != null) 
+                return true;
+            return false;
         }
 
-        bool removeEdge(int vertex1, int vertex2) {
-            return adjacencyList[vertex1].Remove(vertex2);
+        bool RemoveEdge(int vertex1, int vertex2) 
+        {
+            return _adjacencyList[vertex1].Remove(vertex2);
         }
 
-        public int[] getEdgesForVertex(int vertex) {
-            LinkedList<int> list = adjacencyList[vertex];
+        public int[] GetEdgesForVertex(int vertex) 
+        {
+            LinkedList<int> list = _adjacencyList[vertex];
             int[] edgs = new int[list.Count];
             list.CopyTo(edgs, 0);
             return edgs;
         }
 
-        public int[] dijkstraAlgorithm(int startVertex) {
-            int[] distances = new int[size];
-            bool[] used = new bool[size];
+        public int[] DijkstraAlgorithm(int startVertex) 
+        {
+            int[] distances = new int[_size];
+            bool[] used = new bool[_size];
 
-            for (int i = 0; i < size; i++) {
+            for (var i = 0; i < _size; i++) 
+            {
                 distances[i] = int.MaxValue;
             }
             distances[startVertex] = 0;
 
-            for (int i = 0; i < size; i++) {
+            for (var i = 0; i < _size; i++) 
+            {
                 int curr = -1;
-                for (int j = 0; j < size; j++)
+                for (var j = 0; j < _size; j++)
                     if (!used[j] && (curr == -1 || distances[j] < distances[curr]))
                         curr = j;
 
                 if (distances[curr] == int.MaxValue) break;
                 used[curr] = true;
 
-                LinkedList<int> list = adjacencyList[curr];
-                foreach (int vertex in list)
+                LinkedList<int> list = _adjacencyList[curr];
+                foreach (var vertex in list)
                     if (distances[curr] + 1 < distances[vertex])
                         distances[vertex] = distances[curr] + 1;
 
