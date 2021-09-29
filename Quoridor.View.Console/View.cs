9a342a5a38e6de7dc40d8b3b00da8ebcf6e53fcd
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-09-29 09:59:16 +0300

    ✨: add ReadMove method

--- Quoridor.View.Console/View.cs
+++ Quoridor.View.Console/View.cs
@@ -1,7 +1,17 @@
-﻿namespace Quoridor.View
+﻿using System;
+
+namespace Quoridor.View
 {
     internal class View
     {
-
+        public static string ReadMove()
+        {
+            string input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                return "No input. Try again";
+            }
+            return input;
+        }
     }
 }
