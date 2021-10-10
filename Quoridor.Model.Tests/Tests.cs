1877d3b1dfd0a31326de80e20a78da2285d05892
Valentyn-Dominskyi <valentyn2202@gmail.com>
2021-10-10 21:06:53 +0300

    ✅: add first test

--- Quoridor.Model.Tests/Tests.cs
+++ Quoridor.Model.Tests/Tests.cs
@@ -4,10 +4,40 @@ namespace Quoridor.Model.Tests
 {
     public class Tests
     {
-        [Fact]
-        public void Test1()
-        {
+        #region Coordinates
 
+        public QuoridorGame CreateGame()
+        {
+            Board board = new BoardFactory().CreateBoard();
+
+            Cell firstPlayerCell = board.GetStartCellForPlayer
+                ((int)PlayerID.First);
+            Cell[] firstPlayerEndCells = board.GetEndCellsForPlayer
+                (board.GetStartCellForPlayer((int)PlayerID.First));
+
+            Player firstPlayer = new(firstPlayerCell, firstPlayerEndCells);
+
+            Cell secondPlayerCell = board.GetStartCellForPlayer
+                ((int)PlayerID.Second);
+            Cell[] secondPlayerEndCells = board.GetEndCellsForPlayer
+                (board.GetStartCellForPlayer((int)PlayerID.Second));
+
+            Player secondPlayer = new(secondPlayerCell, secondPlayerEndCells);
+
+            QuoridorGame game = new QuoridorGame(firstPlayer, secondPlayer, board);
+
+            return game;
         }
+
+        [Fact]
+        public void CheckGameEnd_GameIsNotEnded_False()
+        {
+            QuoridorGame game = CreateGame();
+
+            Assert.False(game.CheckGameEnd());
+        }
+
+        #endregion Coordinates
+
     }
 }
