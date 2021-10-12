25cca29fd140d9f5d60f85e4c057610593b34540
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-10-12 17:13:30 +0300

    :white_check_mark:: add tests GetPossiGetPossiblePlayersMoves

--- Quoridor.Model.Tests/BoardTests.cs
+++ Quoridor.Model.Tests/BoardTests.cs
@@ -18,8 +18,49 @@ namespace Quoridor.Model.Tests
 
         #endregion CheckBoardFactory
 
+        #region GetPossiGetPossiblePlayersMoves
 
-        
+        [Fact]
+        public void GetPossiGetPossiblePlayersMoves()
+        {
+            Board board = new BoardFactory().CreateBoard();
+            Cell from = board.GetCellByCoordinates(new Coordinates(4, 0));
+            Cell through = Cell.Default;
+            Cell[] actual = board.GetPossiblePlayersMoves(from, through);
+            Cell[] expected = new Cell[] {
+                board.GetCellByCoordinates(new Coordinates(3, 0)),
+            board.GetCellByCoordinates(new Coordinates(4, 1)),
+            board.GetCellByCoordinates(new Coordinates(5, 0))
+            };
+            Assert.Equal(actual.GetLength(0), expected.GetLength(0));
+            foreach (Cell elem in actual)
+            {
+                Assert.Contains<IElement>(elem, expected);
+            }
+        }
+
+        [Fact]
+        public void GetPossiGetPossiblePlayersMoves_WithJump()
+        {
+            Board board = new BoardFactory().CreateBoard();
+            Cell from = board.GetCellByCoordinates(new Coordinates(4, 0));
+            Cell through = board.GetCellByCoordinates(new Coordinates(4, 1));
+            Cell[] actual = board.GetPossiblePlayersMoves(from, through);
+            Cell[] expected = new Cell[] {
+                board.GetCellByCoordinates(new Coordinates(3, 0)),
+            board.GetCellByCoordinates(new Coordinates(4, 1)),
+            board.GetCellByCoordinates(new Coordinates(4, 2)),
+            board.GetCellByCoordinates(new Coordinates(5, 0))
+            };
+            Assert.Equal(actual.GetLength(0), expected.GetLength(0));
+            foreach (Cell elem in actual)
+            {
+                Assert.Contains<IElement>(elem, expected);
+            }
+        }
+
+        #endregion GetPossiGetPossiblePlayersMoves
+
 
     }
 }
