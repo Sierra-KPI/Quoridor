c5b275f9bba57411c713daa0f17e8d199447f80b
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-10-12 17:11:56 +0300

    :white_check_mark:: add test CheckBoardFactory

---
+++ Quoridor.Model.Tests/BoardTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace Quoridor.Model.Tests
+{
+    public class BoardTests
+    {
+
+        #region CheckBoardFactory
+
+        [Fact]
+        public void CheckBoardFactory()
+        {
+            Board board = new BoardFactory().CreateBoard();
+            Assert.Equal(9, board.Size);
+            Assert.Equal(0, board.GetPlacedWalls().GetLength(0));
+        }
+
+        #endregion CheckBoardFactory
+
+
+        
+
+    }
+}
